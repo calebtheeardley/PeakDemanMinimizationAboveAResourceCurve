95738e396b25b118dd20726bbccaf7281120580c
jhb77 <josephbarrows03@gmail.com>
2025-06-19 13:12:40 -0600

    minor changes

--- Code/CPLEX/job_scheduling_lp.py
+++ Code/CPLEX/job_scheduling_lp.py
@@ -22,7 +22,9 @@ start_size = 600
 end_size = 1100
 step_size = 100
 
+final_inexact_values = []
 final_greedy_values = []
+final_naive_values = []
 
 
 for batch_size in range(start_size, end_size, step_size):
@@ -95,18 +97,18 @@ for batch_size in range(start_size, end_size, step_size):
             - For example, the entry (3, 6) in the intervals[0] means that the first job can possibly execute between time steps 3 and 6 (where the start
             time is inclusive and the end time is exclusive)
         """
-        # intervals = [[] for _ in range(len(jobs))]
-        # for i, job in enumerate(jobs):
-        #     # Extract the necessary information from the job object
-        #     release = job['release'] - start_time
-        #     deadline = job['deadline'] - start_time
-        #     duration = job['length']
-        #     num = release
+        intervals = [[] for _ in range(len(jobs))]
+        for i, job in enumerate(jobs):
+            # Extract the necessary information from the job object
+            release = job['release'] - start_time
+            deadline = job['deadline'] - start_time
+            duration = job['length']
+            num = release
 
-        #     # Add the execution intervals to the sublist
-        #     while (num + duration <= deadline):
-        #         intervals[i].append((num, num + duration))
-        #         num += 1
+            # Add the execution intervals to the sublist
+            while (num + duration <= deadline):
+                intervals[i].append((num, num + duration))
+                num += 1
         
 
         """
@@ -188,20 +190,20 @@ for batch_size in range(start_size, end_size, step_size):
             - The height is another word for the amount of the given resource that a job will consistently require while it is running.
             - Therefore, height[0] = 400 means that the first job requires 400 unites of the given resource
         """
-        # naive_heights = [0 for _ in range(num_time_steps)]
-        # for job in jobs:
-        #     aj = job['release'] - start_time
-        #     hj = job['height']
-        #     lj = job['length']
+        naive_heights = [0 for _ in range(num_time_steps)]
+        for job in jobs:
+            aj = job['release'] - start_time
+            hj = job['height']
+            lj = job['length']
 
-        #     for i in range(aj, aj + lj):
-        #         naive_heights[i] += hj
+            for i in range(aj, aj + lj):
+                naive_heights[i] += hj
         
         # Calculate the heights for the different jobs in the greedy array
         greedy_heights = [job['height'] for job in greedy_jobs]
 
         # Iterate through the jobs and add their corresponding heights
-        # height = [job['height'] for job in jobs]
+        height = [job['height'] for job in jobs]
 
 
 
@@ -217,14 +219,14 @@ for batch_size in range(start_size, end_size, step_size):
 
         Objective variable -> Instantiate the variable that will be minimized during the problem's execution
         """
-        # decision_variables = []
-        # for j, interval_set in enumerate(intervals):
-        #     for i, interval in enumerate(interval_set):
-        #         # Add the decision variable and it's corresponding interval to the list
-        #         decision_variables.append({'name' : f'x_{i}_{j}', 'value': interval})
+        decision_variables = []
+        for j, interval_set in enumerate(intervals):
+            for i, interval in enumerate(interval_set):
+                # Add the decision variable and it's corresponding interval to the list
+                decision_variables.append({'name' : f'x_{i}_{j}', 'value': interval})
 
-        # # This is the name of the objective variable that we will minimize
-        # objective_variable = 'd'
+        # This is the name of the objective variable that we will minimize
+        objective_variable = 'd'
 
 
 
@@ -236,26 +238,26 @@ for batch_size in range(start_size, end_size, step_size):
         Finally we have to set up the objective function. In this case, the objective function will be to minimize a variable named 'd'. Where d is initially set to the highest
         point the job schedule curve could possibly be, which is the total sum of the 'heights' list.
         """
-        # # Create the cplex problem
-        # problem = cplex.Cplex()
-        # problem.set_problem_type(cplex.Cplex.problem_type.LP)
-        # problem.set_results_stream(None)
+        # Create the cplex problem
+        problem = cplex.Cplex()
+        problem.set_problem_type(cplex.Cplex.problem_type.LP)
+        problem.set_results_stream(None)
 
-        # # Maximize objective
-        # problem.objective.set_sense(problem.objective.sense.minimize)
+        # Maximize objective
+        problem.objective.set_sense(problem.objective.sense.minimize)
 
-        # # This retrieves the names of all of the decision variables 
-        # names = [variable['name'] for variable in decision_variables] + [objective_variable]
+        # This retrieves the names of all of the decision variables 
+        names = [variable['name'] for variable in decision_variables] + [objective_variable]
 
-        # # these are the other parameters needed to form the basis of the linear programming problem
-        # obj = [0 for _ in range(len(decision_variables))] + [1] # only minimizing d
-        # lb = [0 for _ in range(len(decision_variables))] + [0]
-        # ub = [1 for _ in range(len(decision_variables)) ] + [sum(height)]
+        # these are the other parameters needed to form the basis of the linear programming problem
+        obj = [0 for _ in range(len(decision_variables))] + [1] # only minimizing d
+        lb = [0 for _ in range(len(decision_variables))] + [0]
+        ub = [1 for _ in range(len(decision_variables)) ] + [sum(height)]
 
 
-        # # Establish the problem
-        # types = [problem.variables.type.continuous] * (len(decision_variables)) + [problem.variables.type.continuous]
-        # problem.variables.add(obj=obj, lb=lb, ub=ub, types=types, names=names)
+        # Establish the problem
+        types = [problem.variables.type.continuous] * (len(decision_variables)) + [problem.variables.type.continuous]
+        problem.variables.add(obj=obj, lb=lb, ub=ub, types=types, names=names)
 
 
 
@@ -267,22 +269,22 @@ for batch_size in range(start_size, end_size, step_size):
         This for loop adds the constraints that make it so that a job can run during only one interval. So for each job, aggregate all of the decision variables that correspond to each possible execution interval for that job. 
         Then specify that they all of those decision variables can only add up to one
         """
-        # curr_index = 0
-        # for interval in intervals:
-        #     # Aggregate all the decision variables that belong to one job
-        #     variables = decision_variables[curr_index : curr_index + (len(interval))]
-        #     variables = [v['name'] for v in variables]
-        #     curr_index += len(interval)
+        curr_index = 0
+        for interval in intervals:
+            # Aggregate all the decision variables that belong to one job
+            variables = decision_variables[curr_index : curr_index + (len(interval))]
+            variables = [v['name'] for v in variables]
+            curr_index += len(interval)
             
-        #     # The coefficient of each decision variable is one
-        #     constraints = [1 for _ in range(len(variables))]
+            # The coefficient of each decision variable is one
+            constraints = [1 for _ in range(len(variables))]
 
-        #     # This is saying that the sum of each of the decision variabels can only equal one
-        #     problem.linear_constraints.add(
-        #         lin_expr=[ [ variables, constraints ] ],
-        #         senses=['E'],
-        #         rhs=[1]
-        #     )
+            # This is saying that the sum of each of the decision variabels can only equal one
+            problem.linear_constraints.add(
+                lin_expr=[ [ variables, constraints ] ],
+                senses=['E'],
+                rhs=[1]
+            )
 
 
         """
@@ -292,42 +294,42 @@ for batch_size in range(start_size, end_size, step_size):
         It then aggregates the heights corresponding to the jobs that each decision variable represents. It multiplies 
         those heights by the decision variables. However, the constrain ensures that the total sum is less than the max height d
         """
-        # for i in range(num_time_steps):
-        #     use_variables = []
-        #     use_height = []
+        for i in range(num_time_steps):
+            use_variables = []
+            use_height = []
 
-        #     for variable in decision_variables:
+            for variable in decision_variables:
                 
-        #         # Check the interval times of the corresponding variable
-        #         # Then check if the current timestep falls within that interval
-        #         job_interval_start, job_interval_end = variable['value'][0], variable['value'][1]
+                # Check the interval times of the corresponding variable
+                # Then check if the current timestep falls within that interval
+                job_interval_start, job_interval_end = variable['value'][0], variable['value'][1]
 
-        #         if job_interval_start <= i < job_interval_end:
-        #             job_id = int(variable['name'].split('_')[-1])
+                if job_interval_start <= i < job_interval_end:
+                    job_id = int(variable['name'].split('_')[-1])
 
-        #             use_height.append(height[job_id])
-        #             use_variables.append(variable['name'])
+                    use_height.append(height[job_id])
+                    use_variables.append(variable['name'])
 
-        #     # Add d to the decision variables 
-        #     use_variables.append('d')
-        #     use_height.append(-1)
+            # Add d to the decision variables 
+            use_variables.append('d')
+            use_height.append(-1)
 
-        #     # Add the linear constraint to the problem
-        #     problem.linear_constraints.add(
-        #         lin_expr=[ [ use_variables, use_height ] ],
-        #         senses=['L'],
-        #         rhs=[resources[i]]
-        #     )
+            # Add the linear constraint to the problem
+            problem.linear_constraints.add(
+                lin_expr=[ [ use_variables, use_height ] ],
+                senses=['L'],
+                rhs=[resources[i]]
+            )
 
 
 
         """
         ----- Solve the LP ----- 
         """
-        # print("start solve")
-        # problem.solve()
-        # solution = problem.solution
-        # print("end solve")
+        print("start solve")
+        problem.solve()
+        solution = problem.solution
+        print("end solve")
 
 
 
@@ -339,42 +341,42 @@ for batch_size in range(start_size, end_size, step_size):
         Therefore, we use the values of the decision variables as a probability that that specific interval will run. This means that if a decision variable has a h
         igher value, it's corresponding interval has a higher likelihood of being chosen for the job.
         """
-        # # Loop through each of the jobs and generate a random number
-        # # Choose a decision variable based on the probability of the current value of the decision variables
-        # # Add that chosen variable to a final list so that the overall objective value can be ascertained
-        # final_intervals = []
-        # final_heights = [0 for _ in range(num_time_steps)]
+        # Loop through each of the jobs and generate a random number
+        # Choose a decision variable based on the probability of the current value of the decision variables
+        # Add that chosen variable to a final list so that the overall objective value can be ascertained
+        final_intervals = []
+        final_heights = [0 for _ in range(num_time_steps)]
 
-        # curr_index = 0
-        # # Loop through each job
-        # for job_id in range(len(intervals)):
-        #     # Generate a random number for the job to be used to select a specific interval
-        #     random_num = random.uniform(0, 1)
-        #     probability = 0
+        curr_index = 0
+        # Loop through each job
+        for job_id in range(len(intervals)):
+            # Generate a random number for the job to be used to select a specific interval
+            random_num = random.uniform(0, 1)
+            probability = 0
 
-        #     # Loop through each interval in the job and get the value corresponding to each interval (decision variable)
-        #     # Add the decision variable to the final interval list based on the random number 
-        #     for i, interval in enumerate(intervals[job_id]):
-        #         decision_variable = decision_variables[curr_index]
-        #         decision_value = solution.get_values(decision_variable['name'])
-        #         probability += decision_value
+            # Loop through each interval in the job and get the value corresponding to each interval (decision variable)
+            # Add the decision variable to the final interval list based on the random number 
+            for i, interval in enumerate(intervals[job_id]):
+                decision_variable = decision_variables[curr_index]
+                decision_value = solution.get_values(decision_variable['name'])
+                probability += decision_value
 
-        #         if random_num <= probability and len(final_intervals) <= job_id:
-        #             final_intervals.append(decision_variable)
+                if random_num <= probability and len(final_intervals) <= job_id:
+                    final_intervals.append(decision_variable)
                 
-        #         curr_index += 1
+                curr_index += 1
 
-        # # Generate the height of all of the jobs over the course of all of the time steps
-        # # Do this by iterating through all of the selected job intervals in final_intervals and add their height values 
-        # # to the final_heights arrays. From this we can determine the objective value of d
-        # # simply take the maximum from this height list
-        # for job_id, job in enumerate(final_intervals):
-        #     job_start = job['value'][0]
-        #     job_end = job['value'][1]
-        #     job_height = height[job_id]
+        # Generate the height of all of the jobs over the course of all of the time steps
+        # Do this by iterating through all of the selected job intervals in final_intervals and add their height values 
+        # to the final_heights arrays. From this we can determine the objective value of d
+        # simply take the maximum from this height list
+        for job_id, job in enumerate(final_intervals):
+            job_start = job['value'][0]
+            job_end = job['value'][1]
+            job_height = height[job_id]
 
-        #     for i in range(job_start, job_end):
-        #         final_heights[i] += job_height
+            for i in range(job_start, job_end):
+                final_heights[i] += job_height
 
 
 
@@ -428,19 +430,21 @@ for batch_size in range(start_size, end_size, step_size):
 
         This code calculates the maximum peak above the demand curve for the naive algorithm and the inexact algorithm
         """
-        # objective_value = 0
-        # for i, height in enumerate(final_heights):
-        #     if height - resources[i] > objective_value:
-        #         objective_value = height - resources[i]
+        objective_value = 0
+        for i, height in enumerate(final_heights):
+            if height - resources[i] > objective_value:
+                objective_value = height - resources[i]
 
-        # print("Inexact Objective Value:", objective_value)
+        print("Inexact Objective Value:", objective_value)
+        final_inexact_values.append(objective_value)
 
-        # naive_objective_value = 0
-        # for i, height in enumerate(naive_heights):
-        #     if (height) - resources[i] > naive_objective_value:
-        #         naive_objective_value = (height) - resources[i]
+        naive_objective_value = 0
+        for i, height in enumerate(naive_heights):
+            if (height) - resources[i] > naive_objective_value:
+                naive_objective_value = (height) - resources[i]
 
-        # print("Naive Objective Value:", naive_objective_value)
+        print("Naive Objective Value:", naive_objective_value)
+        final_naive_values.append(naive_objective_value)
 
         greedy_objective_value = 0
         for i, height in enumerate(final_greedy_heights_2):
@@ -454,38 +458,38 @@ for batch_size in range(start_size, end_size, step_size):
         ----- Export the data ----- 
         """
         # Write to a data csv file
-        # data = [
-        #     {"batch_size": batch_size,"trial #": k, "naive obective val": naive_objective_value, "inexact objective val": objective_value}
-        # ]
+        data = [
+            {"batch_size": batch_size,"trial #": k, "naive obective val": naive_objective_value, "inexact objective val": objective_value, "greedy objective val": greedy_objective_value}
+        ]
 
-        # with open("../../Output_Data/Results/inexact_objective_values_600_1000.csv", "a", newline="") as csvfile:
-        #     fieldnames = ['batch_size', 'trial #', 'naive obective val', 'inexact objective val']
-        #     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        with open("../../Output_Data/Results/inexact_objective_values_600_1000.csv", "a", newline="") as csvfile:
+            fieldnames = ['batch_size', 'trial #', 'naive obective val', 'inexact objective val', 'greedy_objective_val']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             
-        #     # Only write the header on the very first trial run
-        #     if batch_size == start_size and k == 0:
-        #         writer.writeheader()
-        #     writer.writerows(data)
+            # Only write the header on the very first trial run
+            if batch_size == start_size and k == 0:
+                writer.writeheader()
+            writer.writerows(data)
 
-        #     csvfile.close()
+            csvfile.close()
 
 
 # Append to the data csv file
-path = "../../Output_Data/Results/inexact_objective_values_600_1000.csv"
-with open(path, "r") as file:
-    reader = csv.reader(file)
-    data = list(reader)
+# path = "../../Output_Data/Results/inexact_objective_values_600_1000.csv"
+# with open(path, "r") as file:
+#     reader = csv.reader(file)
+#     data = list(reader)
 
-# Add the greedy objective value column to the csv
-data[0].append("greedy objective val")
+# # Add the greedy objective value column to the csv
+# data[0].append("greedy objective val")
 
-# Add the new greedy objective values to the column
-for i in range(1, len(data)):
-    data[i].append(final_greedy_values[i - 1])
+# # Add the new greedy objective values to the column
+# for i in range(1, len(data)):
+#     data[i].append(final_greedy_values[i - 1])
 
-# Write those final values to the actual csv
-with open(path, "w", newline="") as file:
-    writer = csv.writer(file)
-    writer.writerows(data)
+# # Write those final values to the actual csv
+# with open(path, "w", newline="") as file:
+#     writer = csv.writer(file)
+#     writer.writerows(data)
 
         
